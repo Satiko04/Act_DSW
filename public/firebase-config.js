96e8ffb9513cb44bd9654c10986ad34cd8c4c67e
Giselli_Satiko_Fujimori <gisellifujimori@gmail.com>
2026-06-14 12:08:34 -0400

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -8,12 +8,12 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 // valores reais em repositórios públicos — troque pelos seus)
 // ─────────────────────────────────────────────────────────────
 const firebaseConfig = {
-    apiKey: "AIzaSyAIiQdFhXXh6XHHj5w01zXbDpwUiqaq_mg",
-    authDomain: "dsw2026-01.firebaseapp.com",
-    projectId: "dsw2026-01",
-    storageBucket: "dsw2026-01.firebasestorage.app",
-    messagingSenderId: "286850502890",
-    appId: "1:286850502890:web:1e8ad29fa8e23196d18793"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
